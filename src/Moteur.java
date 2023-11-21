217cd0ee6a8da0cfab471602c1238192d37f1b61
Loana MOTTAIS <loana.mottais@gmail.com>
2023-11-21 17:08:45 +0100

    Moteur.java (vide)

--- src/Moteur.java
+++ src/Moteur.java
@@ -7,17 +7,6 @@ public class Moteur {
         this.baseFaits=baseFaits;
     }
 
-
-    /*la méthode vérifie si une règle est applicable, pour cela il faut que toutes 
-     * les faits de la règle existe dans la base de fait et soit vraie et on ajoute la conclusion, 
-     * si elle n'existe pas encore dans la base de fait
-     */
-   /* public boolean estApplicable(){
-       for(){
-
-        }
-    }*/
-
     public void chainageAvant(){
 
     }
